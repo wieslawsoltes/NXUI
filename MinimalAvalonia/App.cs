274e6aec132fd7b466079280cc19174b64a277b0
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-09-01 23:06:43 +0200

    Create App.cs

---
+++ MinimalAvalonia/App.cs
@@ -0,0 +1,19 @@
+BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+
+static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<App>().UsePlatformDetect().LogToTrace();
+
+class App : Application {
+    public override void OnFrameworkInitializationCompleted() {
+        Styles.Add(new FluentTheme(new Uri("avares://MinimalAvalonia")) { Mode = FluentThemeMode.Light });
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
+            var mainWindow = new Window {
+                Title = "MinimalAvalonia",
+                Content = new TextBlock {
+                    Text = "MinimalAvalonia"
+                    }
+            };
+            desktop.MainWindow = mainWindow;
+        }
+        base.OnFrameworkInitializationCompleted();
+    }
+}
