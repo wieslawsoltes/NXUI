0c1b956ac6af0bebd3cb4043c5b928cf5c982b9d
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-09-07 09:54:20 +0200

    Use AppBuilderMinimalExtensions

--- MinimalAvalonia/App.cs
+++ MinimalAvalonia/App.cs
@@ -1,16 +1,12 @@
 BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
 
-static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<App>().UsePlatformDetect();
-
-class App : Application {
-    public override void OnFrameworkInitializationCompleted() {
-        Styles.Add(new FluentTheme(new Uri("avares://MinimalAvalonia")));
-        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
-            desktop.MainWindow = new Window {
-                Title = "MinimalAvalonia",
-                Content = new TextBlock { Text = "MinimalAvalonia" }
-            };
-        }
-        base.OnFrameworkInitializationCompleted();
-    }
-}
+static AppBuilder BuildAvaloniaApp() 
+    => AppBuilder.Configure<Application>()
+                 .UsePlatformDetect()
+                 .UseFluentTheme(FluentThemeMode.Dark)
+                 .WithClassicDesktopStyleApplicationLifetime(desktop => {
+                     desktop.MainWindow = new Window {
+                         Title = "MinimalAvalonia",
+                         Content = new TextBlock { Text = "MinimalAvalonia" }
+                     };
+                 });
---
+++ MinimalAvalonia/AppBuilderMinimalExtensions.cs
@@ -0,0 +1,17 @@
+﻿public static class AppBuilderMinimalExtensions
+{
+    public static TAppBuilder UseFluentTheme<TAppBuilder>(this TAppBuilder builder, FluentThemeMode mode = FluentThemeMode.Light)
+        where TAppBuilder : AppBuilderBase<TAppBuilder>, new() {
+        return builder.AfterSetup(_ =>
+            builder.Instance.Styles.Add(new FluentTheme(new Uri($"avares://{System.Reflection.Assembly.GetExecutingAssembly().GetName()}")) { Mode = mode }));
+    }
+
+    public static TAppBuilder WithClassicDesktopStyleApplicationLifetime<TAppBuilder>(this TAppBuilder builder, Action<IClassicDesktopStyleApplicationLifetime> callback)
+        where TAppBuilder : AppBuilderBase<TAppBuilder>, new() {
+        return builder.AfterSetup(_ => {
+            if (builder.Instance.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
+                callback?.Invoke(desktop);
+            }
+        });
+    }
+}
