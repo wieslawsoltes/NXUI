bf7a8f7aee62498848f866a5ff594e0fd4af6589
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-09-07 08:18:17 +0200

    Use default mode for fluent theme

--- MinimalAvalonia/App.cs
+++ MinimalAvalonia/App.cs
@@ -4,7 +4,7 @@ static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<App>().UsePlatformD
 
 class App : Application {
     public override void OnFrameworkInitializationCompleted() {
-        Styles.Add(new FluentTheme(new Uri("avares://MinimalAvalonia")) { Mode = FluentThemeMode.Light });
+        Styles.Add(new FluentTheme(new Uri("avares://MinimalAvalonia")));
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
             desktop.MainWindow = new Window {
                 Title = "MinimalAvalonia",
