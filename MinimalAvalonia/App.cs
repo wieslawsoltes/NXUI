4e2ebc29c7db1ad7e8dcea94506709ba84bd4b79
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-09-07 11:39:23 +0200

    Update App.cs

--- MinimalAvalonia/App.cs
+++ MinimalAvalonia/App.cs
@@ -2,7 +2,9 @@ AppBuilder.Configure<Application>()
           .UsePlatformDetect()
           .UseFluentTheme()
           .StartWithClassicDesktopLifetime(desktop => {
-              var window = new Window { Title = "Minimal Avalonia" };
-              window.Content = new TextBox { [!!TextBlock.TextProperty] = window[!!Window.TitleProperty] };
-              desktop.MainWindow = window;
+              var tb = new TextBox() {  Text = "Minimal Avalonia" };
+              desktop.MainWindow = new Window {
+                  Content = tb,
+                  [!!Window.TitleProperty] = tb.GetObservable(TextBox.TextProperty).Select(x => x.ToUpper()).ToBinding()
+              };
           }, args);
