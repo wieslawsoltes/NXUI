fb11ef385d7e07635fe60f16080bcd110c238ec0
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-09-07 08:15:05 +0200

    Remove local variable

--- MinimalAvalonia/App.cs
+++ MinimalAvalonia/App.cs
@@ -6,13 +6,12 @@ class App : Application {
     public override void OnFrameworkInitializationCompleted() {
         Styles.Add(new FluentTheme(new Uri("avares://MinimalAvalonia")) { Mode = FluentThemeMode.Light });
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
-            var mainWindow = new Window {
+            desktop.MainWindow = new Window {
                 Title = "MinimalAvalonia",
                 Content = new TextBlock {
                     Text = "MinimalAvalonia"
                 }
             };
-            desktop.MainWindow = mainWindow;
         }
         base.OnFrameworkInitializationCompleted();
     }
