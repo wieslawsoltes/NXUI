86c9cfedc19f4c3ee3f5c40f8f6571c601b9cd78
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-05-06 14:22:04 +0200

    Create StackPanelExtensions.cs

---
+++ MinimalAvalonia/StackPanelExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace MinimalAvalonia;
+
+public static class StackPanelExtensions
+{
+    public static T Spacing<T>(this T stackPanel, double spacing) where T : StackPanel
+    {
+        stackPanel[StackPanel.SpacingProperty] = spacing;
+        return stackPanel;
+    }
+
+    public static T OrientationHorizontal<T>(this T stackPanel) where T : StackPanel
+    {
+        stackPanel[StackPanel.OrientationProperty] = Orientation.Horizontal;
+        return stackPanel;
+    }
+
+    public static T OrientationVertical<T>(this T stackPanel) where T : StackPanel
+    {
+        stackPanel[StackPanel.OrientationProperty] = Orientation.Vertical;
+        return stackPanel;
+    }
+}
