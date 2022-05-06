a30ac497e137c71c50c06c577abbb1f08f33f05c
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-05-06 14:22:15 +0200

    Create TextBlockExtensions.cs

---
+++ MinimalAvalonia/TextBlockExtensions.cs
@@ -0,0 +1,23 @@
+namespace MinimalAvalonia;
+
+public static class TextBlockExtensions
+{
+    public static T Text<T>(this T textBlock, string text) where T : TextBlock
+    {
+        textBlock[TextBlock.TextProperty] = text;
+        return textBlock;
+    }
+
+    // TODO:
+    // FontFamilyProperty
+    // FontSizeProperty
+    // FontStyleProperty
+    // FontWeightProperty
+    // ForegroundProperty
+    // LineHeightProperty
+    // MaxLinesProperty
+    // TextAlignmentProperty
+    // TextWrappingProperty
+    // TextTrimmingProperty
+    // TextDecorationsProperty
+}
