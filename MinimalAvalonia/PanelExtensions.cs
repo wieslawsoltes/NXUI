3897ef6b13c11411be95ec1e0f085b487d2fe55d
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-05-06 14:21:52 +0200

    Create PanelExtensions.cs

---
+++ MinimalAvalonia/PanelExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace MinimalAvalonia;
+
+public static class PanelExtensions
+{
+    public static T Background<T>(this T panel, IBrush background) where T : Panel
+    {
+        panel[Panel.BackgroundProperty] = background;
+        return panel;
+    }
+
+    public static T Children<T>(this T panel, Control child) where T : Panel
+    {
+        panel.Children.Add(child);
+        return panel;
+    }
+
+    public static T Children<T>(this T panel, params IControl[] children) where T : Panel
+    {
+        panel.Children.AddRange(children);
+        return panel;
+    }
+}
