06ece176d956b4d7ef6204b0bc430b7360322506
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-05-06 19:16:17 +0200

    Update Program.cs

--- 7GUIs/Counter/Program.cs
+++ 7GUIs/Counter/Program.cs
@@ -5,27 +5,17 @@
         var count = new BehaviorSubject<int>(0);
         new Button()
             .OnClick(o => o.Subscribe(_ => count.OnNext(count.Value + 1)))
-            .Content("Count")
-            .Ref(out var button);
+            .Content("Count").Ref(out var button);
         new Label()
-            .HorizontalAlignmentCenter()
-            .VerticalAlignmentCenter()
-            .Content(count.ToBinding())
-            .Ref(out var label);
+            .HorizontalAlignmentCenter().VerticalAlignmentCenter()
+            .Content(count.ToBinding()).Ref(out var label);
         new StackPanel()
-            .OrientationHorizontal()
-            .Spacing(12)
-            .HorizontalAlignmentCenter()
-            .VerticalAlignmentCenter()
-            .Children(label, button)
-            .Ref(out var stackPanel);
+            .OrientationHorizontal().Spacing(12)
+            .HorizontalAlignmentCenter().VerticalAlignmentCenter()
+            .Children(label, button).Ref(out var stackPanel);
         new Window()
             .Title("Counter")
-            .Padding(12)
-            .SizeToContentManual()
-            .Width(300)
-            .Height(200)
-            .Content(stackPanel)
-            .Ref(out var window);
+            .Padding(12).SizeToContentManual().Width(300).Height(200)
+            .Content(stackPanel).Ref(out var window);
         desktop.MainWindow = window;
     }, args);
