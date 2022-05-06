311478e7d62eebbf76f1fb56763016733a091871
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-05-06 10:40:36 +0200

    Update EventsExtensions.cs

--- MinimalAvalonia/EventsExtensions.cs
+++ MinimalAvalonia/EventsExtensions.cs
@@ -4,6 +4,8 @@ public static class EventsExtensions
 {
     public static IObservable<RoutedEventArgs> OnClick(this Button button)
     {
-        return Observable.FromEventPattern<EventHandler<RoutedEventArgs>, RoutedEventArgs>(h => button.Click += h, h => button.Click -= h).Select(x => x.EventArgs);
+        return Observable
+            .FromEventPattern<EventHandler<RoutedEventArgs>, RoutedEventArgs>(h => button.Click += h, h => button.Click -= h)
+            .Select(x => x.EventArgs);
     }
 }
