258381b887bed900f92e12079474bd1fa0f52a7d
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-05-06 14:22:09 +0200

    Create TemplatedControlExtensions.cs

---
+++ MinimalAvalonia/TemplatedControlExtensions.cs
@@ -0,0 +1,27 @@
+namespace MinimalAvalonia;
+
+public static class TemplatedControlExtensions
+{
+    public static T Padding<T>(this T templatedControl, double uniformLength) where T : TemplatedControl
+    {
+        templatedControl[TemplatedControl.PaddingProperty] = new Thickness(uniformLength);
+        return templatedControl;
+    }
+
+    public static T Padding<T>(this T templatedControl, double horizontal, double vertical) where T : TemplatedControl
+    {
+        templatedControl[TemplatedControl.PaddingProperty] = new Thickness(horizontal, vertical);
+        return templatedControl;
+    }
+
+    public static T Padding<T>(this T templatedControl, double left, double top, double right, double bottom) where T : TemplatedControl
+    {
+        templatedControl[TemplatedControl.PaddingProperty] = new Thickness(left, top, right, bottom);
+        return templatedControl;
+    }
+
+    // TODO:
+    // TemplateProperty
+    // IsTemplateFocusTargetProperty
+    // TemplateAppliedEvent
+}
