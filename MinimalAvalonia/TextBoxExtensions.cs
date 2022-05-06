7b14c483d6296a38f4ac94b286b57f620b2884d5
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-05-06 14:22:20 +0200

    Create TextBoxExtensions.cs

---
+++ MinimalAvalonia/TextBoxExtensions.cs
@@ -0,0 +1,32 @@
+namespace MinimalAvalonia;
+
+public static class TextBoxExtensions
+{
+    public static T Text<T>(this T textBox, string text) where T : TextBox
+    {
+        textBox[TextBox.TextProperty] = text;
+        return textBox;
+    }
+
+    // TODO:
+    // AcceptsReturnProperty
+    // AcceptsTabProperty
+    // CaretIndexProperty
+    // IsReadOnlyProperty
+    // PasswordCharProperty
+    // SelectionBrushProperty
+    // SelectionForegroundBrushProperty
+    // CaretBrushProperty
+    // SelectionStartProperty
+    // SelectionEndProperty
+    // MaxLengthProperty
+    // WatermarkProperty
+    // UseFloatingWatermarkProperty
+    // NewLineProperty
+    // InnerLeftContentProperty
+    // InnerRightContentProperty
+    // RevealPasswordProperty
+    // CopyingToClipboardEvent
+    // CuttingToClipboardEvent
+    // PastingFromClipboardEvent
+}
