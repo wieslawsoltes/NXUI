838bbf8e6260078c8a8ac8d4d8c40db37a802f49
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-05-06 14:22:29 +0200

    Create WindowExtensions.cs

---
+++ MinimalAvalonia/WindowExtensions.cs
@@ -0,0 +1,49 @@
+﻿namespace MinimalAvalonia;
+
+public static class WindowExtensions
+{
+    public static Window SizeToContentManual(this Window window)
+    {
+        window[Window.SizeToContentProperty] = SizeToContent.Manual;
+        return window;
+    }
+
+    public static Window SizeToContentWidth(this Window window)
+    {
+        window[Window.SizeToContentProperty] = SizeToContent.Width;
+        return window;
+    }
+
+    public static Window SizeToContentHeight(this Window window)
+    {
+        window[Window.SizeToContentProperty] = SizeToContent.Height;
+        return window;
+    }
+
+    public static Window SizeToContentWidthAndHeight(this Window window)
+    {
+        window[Window.SizeToContentProperty] = SizeToContent.WidthAndHeight;
+        return window;
+    }
+
+    public static Window Title(this Window window, string title)
+    {
+        window.Title = title;
+        return window;
+    }
+
+    // TODO:
+    // HasSystemDecorationsProperty
+    // ExtendClientAreaToDecorationsHintProperty
+    // ExtendClientAreaChromeHintsProperty
+    // ExtendClientAreaTitleBarHeightHintProperty
+    // SystemDecorationsProperty
+    // ShowActivatedProperty
+    // ShowInTaskbarProperty
+    // WindowStateProperty
+    // IconProperty
+    // WindowStartupLocationProperty
+    // CanResizeProperty
+    // WindowClosedEvent
+    // WindowOpenedEvent
+}
