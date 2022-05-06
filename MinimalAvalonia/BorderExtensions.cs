ed3db811d8a20be7555c2074a9679435c7dccd5a
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-05-06 14:20:55 +0200

    Create BorderExtensions.cs

---
+++ MinimalAvalonia/BorderExtensions.cs
@@ -0,0 +1,42 @@
+namespace MinimalAvalonia;
+
+public static class BorderExtensions
+{
+    public static T Background<T>(this T border, IBrush background) where T : Border
+    {
+        border[Border.BackgroundProperty] = background;
+        return border;
+    }
+
+    public static T BorderBrush<T>(this T border, IBrush borderBrush) where T : Border
+    {
+        border[Border.BorderBrushProperty] = borderBrush;
+        return border;
+    }
+
+    public static T BorderThickness<T>(this T border, double uniformLength) where T : Border
+    {
+        border[Border.BorderThicknessProperty] = new Thickness(uniformLength);
+        return border;
+    }
+
+    public static T BorderThickness<T>(this T border, double horizontal, double vertical) where T : Border
+    {
+        border[Border.BorderThicknessProperty] = new Thickness(horizontal, vertical);
+        return border;
+    }
+
+    public static T BorderThickness<T>(this T border, double left, double top, double right, double bottom) where T : Border
+    {
+        border[Border.BorderThicknessProperty] = new Thickness(left, top, right, bottom);
+        return border;
+    }
+    
+    // TODO:
+    // CornerRadiusProperty
+    // BoxShadowProperty
+    // BorderDashOffsetProperty
+    // BorderDashArrayProperty
+    // BorderLineCapProperty
+    // BorderLineJoinProperty
+}
