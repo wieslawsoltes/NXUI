30f76b924fe39378c9c9c98231302842426dbc27
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-05-06 21:10:38 +0200

    Add Counters program

---
+++ 7GUIs/Counters/Program.cs
@@ -0,0 +1,26 @@
+﻿AppBuilder.Configure<Application>()
+    .UsePlatformDetect()
+    .UseFluentTheme()
+    .StartWithClassicDesktopLifetime(desktop => {
+        var counters = Enumerable.Range(0, 5).Select(i => new BehaviorSubject<int>(i));
+        new ItemsControl()
+            .ItemTemplate(new FuncDataTemplate<BehaviorSubject<int>>((count, ns) => {
+                new Button()
+                    .OnClick(o => o.Subscribe(_ => count.OnNext(count.Value + 1)))
+                    .Content("Count").Ref(out var button);
+                new Label()
+                    .HorizontalAlignmentCenter().VerticalAlignmentCenter()
+                    .Content(count.ToBinding()).Ref(out var label);
+                return new StackPanel()
+                    .OrientationHorizontal().Spacing(12)
+                    .HorizontalAlignmentCenter().VerticalAlignmentCenter()
+                    .Children(label, button);
+            }, true))
+            .Items(counters)
+            .Ref(out var itemsControl);
+        new Window()
+            .Title("Counter")
+            .Padding(12).SizeToContentManual().Width(300).Height(300)
+            .Content(itemsControl).Ref(out var window);
+        desktop.MainWindow = window;
+    }, args);
