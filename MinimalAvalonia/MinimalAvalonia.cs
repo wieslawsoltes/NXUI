091c63e9dc2421b06e9576ce32b01de626a1a852
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-05-07 23:16:50 +0200

    Create MinimalAvalonia.cs

---
+++ MinimalAvalonia/MinimalAvalonia.cs
@@ -0,0 +1,84 @@
+namespace MinimalAvalonia;
+
+public static class MinimalAvalonia
+{
+    public static Border Border()
+    {
+        return new Border();
+    }
+
+    public static Button Button()
+    {
+        return new Button();
+    }
+
+    public static ContentControl ContentControl()
+    {
+        return new ContentControl();
+    }
+
+    public static Decorator Decorator()
+    {
+        return new Decorator();
+    }
+
+    public static HeaderedContentControl HeaderedContentControl()
+    {
+        return new HeaderedContentControl();
+    }
+
+    public static ItemsControl ItemsControl()
+    {
+        return new ItemsControl();
+    }
+
+    public static Label Label()
+    {
+        return new Label();
+    }
+
+    public static Layoutable Layoutable()
+    {
+        return new Layoutable();
+    }
+
+    public static Panel Panel()
+    {
+        return new Panel();
+    }
+
+    public static StackPanel StackPanel()
+    {
+        return new StackPanel();
+    }
+
+    public static TabControl TabControl()
+    {
+        return new TabControl();
+    }
+
+    public static TabItem TabItem()
+    {
+        return new TabItem();
+    }
+
+    public static TemplatedControl TemplatedControl()
+    {
+        return new TemplatedControl();
+    }
+
+    public static TextBlock TextBlock()
+    {
+        return new TextBlock();
+    }
+
+    public static TextBox TextBox()
+    {
+        return new TextBox();
+    }
+
+    public static Window Window()
+    {
+        return new Window();
+    }
+}
