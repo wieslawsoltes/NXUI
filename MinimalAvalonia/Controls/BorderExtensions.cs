151874c4c192f8c202fe5803430995d35b60e561
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-05-07 23:31:07 +0200

    Update BorderExtensions.cs

--- MinimalAvalonia/Controls/BorderExtensions.cs
+++ MinimalAvalonia/Controls/BorderExtensions.cs
@@ -31,12 +31,52 @@ public static class BorderExtensions
         border[Border.BorderThicknessProperty] = new Thickness(left, top, right, bottom);
         return border;
     }
-    
-    // TODO:
-    // CornerRadiusProperty
-    // BoxShadowProperty
-    // BorderDashOffsetProperty
-    // BorderDashArrayProperty
-    // BorderLineCapProperty
-    // BorderLineJoinProperty
+
+    public static T CornerRadius<T>(this T border, double uniformRadius) where T : Border
+    {
+        border[Border.CornerRadiusProperty] = new CornerRadius(uniformRadius);
+        return border;
+    }
+
+    public static T CornerRadius<T>(this T border, double top, double bottom) where T : Border
+    {
+        border[Border.CornerRadiusProperty] = new CornerRadius(top, bottom);
+        return border;
+    }
+
+    public static T CornerRadius<T>(this T border, double topLeft, double topRight, double bottomRight, double bottomLeft) where T : Border
+    {
+        border[Border.CornerRadiusProperty] = new CornerRadius(topLeft, topRight, bottomRight, bottomLeft);
+        return border;
+    }
+
+    public static T BoxShadow<T>(this T border, BoxShadows boxShadows) where T : Border
+    {
+        border[Border.BoxShadowProperty] = boxShadows;
+        return border;
+    }
+
+    public static T BorderDashOffset<T>(this T border, double borderDashOffset) where T : Border
+    {
+        border[Border.BorderDashOffsetProperty] = borderDashOffset;
+        return border;
+    }
+
+    public static T BorderDashArray<T>(this T border, AvaloniaList<double>? borderDashArray) where T : Border
+    {
+        border[Border.BorderDashArrayProperty] = borderDashArray;
+        return border;
+    }
+
+    public static T BorderLineCap<T>(this T border, PenLineCap borderLineCap) where T : Border
+    {
+        border[Border.BorderLineCapProperty] = borderLineCap;
+        return border;
+    }
+
+    public static T BorderLineJoin<T>(this T border, PenLineJoin borderLineJoin) where T : Border
+    {
+        border[Border.BorderLineJoinProperty] = borderLineJoin;
+        return border;
+    }
 }
