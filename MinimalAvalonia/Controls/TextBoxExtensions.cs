6cbb31586352fdf7cf72995572823166497b41e6
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-05-07 23:16:41 +0200

    Update TextBoxExtensions.cs

--- MinimalAvalonia/Controls/TextBoxExtensions.cs
+++ MinimalAvalonia/Controls/TextBoxExtensions.cs
@@ -2,6 +2,30 @@ namespace MinimalAvalonia.Controls;
 
 public static class TextBoxExtensions
 {
+    public static T AcceptsReturn<T>(this T textBox, bool acceptsReturn) where T : TextBox
+    {
+        textBox[TextBox.AcceptsReturnProperty] = acceptsReturn;
+        return textBox;
+    }
+
+    public static T AcceptsTab<T>(this T textBox, bool acceptsTab) where T : TextBox
+    {
+        textBox[TextBox.AcceptsTabProperty] = acceptsTab;
+        return textBox;
+    }
+
+    public static T CaretIndex<T>(this T textBox, int caretIndex) where T : TextBox
+    {
+        textBox[TextBox.CaretIndexProperty] = caretIndex;
+        return textBox;
+    }
+
+    public static T IsReadOnly<T>(this T textBox, bool isReadOnly) where T : TextBox
+    {
+        textBox[TextBox.IsReadOnlyProperty] = isReadOnly;
+        return textBox;
+    }
+
     public static T Text<T>(this T textBox, string text) where T : TextBox
     {
         textBox[TextBox.TextProperty] = text;
@@ -9,10 +33,6 @@ public static class TextBoxExtensions
     }
 
     // TODO:
-    // AcceptsReturnProperty
-    // AcceptsTabProperty
-    // CaretIndexProperty
-    // IsReadOnlyProperty
     // PasswordCharProperty
     // SelectionBrushProperty
     // SelectionForegroundBrushProperty
