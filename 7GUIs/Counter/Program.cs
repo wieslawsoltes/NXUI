5c2aa2e0949edeca0d5a607d81beae15a363ba8d
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-05-09 13:17:12 +0200

    Update Program.cs

--- 7GUIs/Counter/Program.cs
+++ 7GUIs/Counter/Program.cs
@@ -1,19 +1,21 @@
-﻿AppBuilder.Configure<Application>()
+﻿var count = new BehaviorSubject<int>(0);
+
+Window Build() 
+    => Window()
+        .Title("Counter").Padding(12).SizeToContentManual().Width(300).Height(200)
+        .Content(
+            StackPanel()
+                .OrientationHorizontal().Spacing(12)
+                .HorizontalAlignmentCenter().VerticalAlignmentCenter()
+                .Children(
+                    Label()
+                        .HorizontalAlignmentCenter().VerticalAlignmentCenter()
+                        .Content(count.ToBinding()),
+                    Button()
+                        .OnClick(o => o.Subscribe(_ => count.OnNext(count.Value + 1)))
+                        .Content("Count")));
+
+AppBuilder.Configure<Application>()
     .UsePlatformDetect()
     .UseFluentTheme()
-    .StartWithClassicDesktopLifetime(() => {
-        var count = new BehaviorSubject<int>(0);
-        return Window()
-            .Title("Counter").Padding(12).SizeToContentManual().Width(300).Height(200)
-            .Content(
-                StackPanel()
-                    .OrientationHorizontal().Spacing(12)
-                    .HorizontalAlignmentCenter().VerticalAlignmentCenter()
-                    .Children(
-                        Label()
-                            .HorizontalAlignmentCenter().VerticalAlignmentCenter()
-                            .Content(count.ToBinding()), 
-                        Button()
-                            .OnClick(o => o.Subscribe(_ => count.OnNext(count.Value + 1)))
-                            .Content("Count")));
-    }, args);
+    .StartWithClassicDesktopLifetime(Build, args);
