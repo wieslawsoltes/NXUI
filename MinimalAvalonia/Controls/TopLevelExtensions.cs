fae4e9ce9eaa6f57b7f0fd131e89c6cb38a01131
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-05-10 00:26:43 +0200

    Create TopLevelExtensions.cs

---
+++ MinimalAvalonia/Controls/TopLevelExtensions.cs
@@ -0,0 +1,12 @@
+namespace MinimalAvalonia.Controls;
+
+public static class TopLevelExtensions
+{
+    // TODO:
+    // ClientSizeProperty
+    // FrameSizeProperty
+    // PointerOverElementProperty
+    // TransparencyLevelHintProperty
+    // ActualTransparencyLevelProperty
+    // TransparencyBackgroundFallbackProperty
+}
