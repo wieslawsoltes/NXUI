16f69638e36f44a2238d0034532d6e80fd374132
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-05-09 22:27:57 +0200

    Create AnimatableExtensions.cs

---
+++ MinimalAvalonia/Controls/AnimatableExtensions.cs
@@ -0,0 +1,30 @@
+namespace MinimalAvalonia.Controls;
+
+public static class AnimatableExtensions
+{
+    public static T Clock<T>(this T animatable, IClock clock) where T : Animatable
+    {
+        animatable[Avalonia.Animation.Animatable.ClockProperty] = clock;
+        return animatable;
+    }
+
+    public static T Transitions<T>(this T animatable, Transitions? transitions) where T : Animatable
+    {
+        animatable[Avalonia.Animation.Animatable.TransitionsProperty] = transitions;
+        return animatable;
+    }
+
+    public static T Transitions<T>(this T animatable, params ITransition[] transitions) where T : Animatable
+    {
+        if (animatable.Transitions is { })
+        {
+            animatable.Transitions.AddRange(transitions);
+        }
+        else
+        {
+            animatable.Transitions = new Transitions();
+            animatable.Transitions.AddRange(transitions);
+        }
+        return animatable;
+    }
+}
