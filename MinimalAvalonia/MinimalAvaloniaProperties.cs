da41fe20645320dc14d652e0c0bc1234100d65e9
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-05-09 20:36:02 +0200

    Update MinimalAvaloniaProperties.cs

--- MinimalAvalonia/MinimalAvaloniaProperties.cs
+++ MinimalAvalonia/MinimalAvaloniaProperties.cs
@@ -5,15 +5,63 @@ namespace MinimalAvalonia;
 [SuppressMessage("ReSharper", "InconsistentNaming")]
 public static class MinimalAvaloniaProperties
 {
-    public static readonly StyledProperty<bool> VisualClipToBounds = Avalonia.Visual.ClipToBoundsProperty;
+    // Animation
+
+    // AnimationSetter
+
+    // Border
+
+    // Button
+
+    // ContentControl
 
     public static readonly StyledProperty<object> ContentControlContent = Avalonia.Controls.ContentControl.ContentProperty;
 
+    // Control
+
+    // Decorator
+
+    // Easing
+
+    // HeaderedContentControl
+
+    // ItemsControl
+
+    // KeyFrame
+
+    // Label
+
+    // Layoutable
+
+    // Panel
+
+    // StackPanel
+
+    // StyledElement
+
+    // Style
+
+    // TabControl
+
+    // TabItem
+
+    // TemplatedControl
+
     public static readonly StyledProperty<IBrush?> TemplatedControlBackground = Avalonia.Controls.Primitives.TemplatedControl.BackgroundProperty;
+
+    // TextBlock
+
+    public static readonly DirectProperty<TextBlock, string> TextBlockText = Avalonia.Controls.TextBlock.TextProperty;
+
+    // TextBox
 
     public static readonly DirectProperty<TextBox, string> TextBoxText = Avalonia.Controls.TextBox.TextProperty;
 
-    public static readonly DirectProperty<TextBlock, string> TextBlockText = Avalonia.Controls.TextBlock.TextProperty;
+    // Visual
+
+    public static readonly StyledProperty<bool> VisualClipToBounds = Avalonia.Visual.ClipToBoundsProperty;
+
+    // Window
 
     public static readonly StyledProperty<string> WindowTitle = Avalonia.Controls.Window.TitleProperty;
 }
