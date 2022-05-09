acc571d1d4757a12ef03725ee8b1c3636cce7ffb
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-05-09 13:14:50 +0200

    Update Program.cs

--- 7GUIs/Counter/Program.cs
+++ 7GUIs/Counter/Program.cs
@@ -1,21 +1,19 @@
 ﻿AppBuilder.Configure<Application>()
     .UsePlatformDetect()
     .UseFluentTheme()
-    .StartWithClassicDesktopLifetime(desktop => {
+    .StartWithClassicDesktopLifetime(() => {
         var count = new BehaviorSubject<int>(0);
-        Button()
-            .OnClick(o => o.Subscribe(_ => count.OnNext(count.Value + 1)))
-            .Content("Count").Ref(out var button);
-        Label()
-            .HorizontalAlignmentCenter().VerticalAlignmentCenter()
-            .Content(count.ToBinding()).Ref(out var label);
-        StackPanel()
-            .OrientationHorizontal().Spacing(12)
-            .HorizontalAlignmentCenter().VerticalAlignmentCenter()
-            .Children(label, button).Ref(out var stackPanel);
-        Window()
-            .Title("Counter")
-            .Padding(12).SizeToContentManual().Width(300).Height(200)
-            .Content(stackPanel).Ref(out var window);
-        desktop.MainWindow = window;
+        return Window()
+            .Title("Counter").Padding(12).SizeToContentManual().Width(300).Height(200)
+            .Content(
+                StackPanel()
+                    .OrientationHorizontal().Spacing(12)
+                    .HorizontalAlignmentCenter().VerticalAlignmentCenter()
+                    .Children(
+                        Label()
+                            .HorizontalAlignmentCenter().VerticalAlignmentCenter()
+                            .Content(count.ToBinding()), 
+                        Button()
+                            .OnClick(o => o.Subscribe(_ => count.OnNext(count.Value + 1)))
+                            .Content("Count")));
     }, args);
