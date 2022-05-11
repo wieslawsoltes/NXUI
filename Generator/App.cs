7a0e0421959b0f85fc9a2f36ef08fde7f6860346
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-05-11 07:27:20 +0200

    Refactor

---
+++ Generator/App.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+
+namespace Generator;
+
+internal class App : Application
+{
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.MainWindow = new Window();
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
--- Generator/Program.cs
+++ Generator/Program.cs
@@ -1,40 +1,18 @@
 ﻿using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Themes.Fluent;
 using Generator;
 
-try
-{
-    AppBuilder.Configure<App>()
-        .UsePlatformDetect()
-        .AfterSetup(x =>
-        {
-            x.Instance.Styles.Add(
-                new FluentTheme(new Uri($"avares://{System.Reflection.Assembly.GetExecutingAssembly().GetName()}"))
-                {
-                    Mode = FluentThemeMode.Light
-                });
-
-            ExtensionsGenerator.Generate();
-        })
-        //.StartWithClassicDesktopLifetime(args);
-        .SetupWithoutStarting();
-}
-catch
-{
-    // ignored
-}
-
-internal class App : Application
-{
-    public override void OnFrameworkInitializationCompleted()
+AppBuilder.Configure<App>()
+    .UsePlatformDetect()
+    .AfterSetup(x =>
     {
-        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        var theme = new FluentTheme(new Uri($"avares://{System.Reflection.Assembly.GetExecutingAssembly().GetName()}"))
         {
-            desktop.MainWindow = new Window();
-        }
+            Mode = FluentThemeMode.Light
+        };
+        x.Instance.Styles.Add(theme);
 
-        base.OnFrameworkInitializationCompleted();
-    }
-}
+        ExtensionsGenerator.Generate();
+    })
+    .SetupWithoutStarting();
+    //.StartWithClassicDesktopLifetime(args);
