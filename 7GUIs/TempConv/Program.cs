e7993cb38de7e1f1a288932554d48470e884315e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-05-13 00:18:12 +0200

    Use OnText

--- 7GUIs/TempConv/Program.cs
+++ 7GUIs/TempConv/Program.cs
@@ -2,48 +2,40 @@
 var fahrenheit = new Subject<string?>();
 
 Window Build() 
-{
-    Window(out var window)
+    => Window()
         .Title("TempConv").Padding(12).Width(350).Height(200)
         .Content(
             StackPanel()
                 .OrientationHorizontal().Spacing(12).HorizontalAlignmentCenter().VerticalAlignmentCenter()
                 .Children(
-                    TextBox(out var tc)
-                        .Text(celsius),
+                    TextBox()
+                        .Text(celsius)
+                        .OnText(o => o.Subscribe(x => {
+                            if (x is null) return;
+                            if (!double.TryParse(x, out var c))
+                            {
+                                celsius.OnError(new Exception());
+                                return;
+                            }
+                            var f = Math.Round(c * (9d / 5d) + 32d);
+                            fahrenheit.OnNext($"{f}");
+                            Debug.WriteLine($"Celsius = {x}");
+                        })),
                     Label()
                         .HorizontalAlignmentCenter().VerticalAlignmentCenter()
                         .Content("Celsius = "),
-                    TextBox(out var tf)
-                        .Text(fahrenheit),
+                    TextBox()
+                        .Text(fahrenheit)
+                        .OnText(o => o.Subscribe(x => {
+                            if (x is null) return;
+                            if (!double.TryParse(x, out var f)) return;
+                            var c = Math.Round((f - 32d) * (5d / 9d));
+                            celsius.OnNext($"{c}");
+                            Debug.WriteLine($"Fahrenheit = {f}");
+                        })),
                     Label()
                         .HorizontalAlignmentCenter().VerticalAlignmentCenter()
                         .Content("Fahrenheit")));
-
-    tc.ObserveText().Subscribe(x =>
-    {
-        if (x is null) return;
-        if (!double.TryParse(x, out var c))
-        {
-            celsius.OnError(new Exception());
-            return;
-        }
-        var f = Math.Round(c * (9d / 5d) + 32d);
-        fahrenheit.OnNext($"{f}");
-        Debug.WriteLine($"Celsius = {x}");
-    });
-
-    tf.ObserveText().Subscribe(x =>
-    {
-        if (x is null) return;
-        if (!double.TryParse(x, out var f)) return;
-        var c = Math.Round((f - 32d) * (5d / 9d));
-        celsius.OnNext($"{c}");
-        Debug.WriteLine($"Fahrenheit = {f}");
-    });
-
-    return window;
-}
 
 AppBuilder.Configure<Application>()
     .UsePlatformDetect()
