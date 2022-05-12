d94aabdaf0e4c4e2fafc2f0907a12de1cf77abd2
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-05-12 13:12:00 +0200

    Update Program.cs

--- 7GUIs/Counters/Program.cs
+++ 7GUIs/Counters/Program.cs
@@ -3,24 +3,23 @@
     .UseFluentTheme()
     .StartWithClassicDesktopLifetime(desktop => {
         var counters = Enumerable.Range(0, 5).Select(i => new BehaviorSubject<int>(i));
-        ItemsControl()
-            .ItemTemplate(new FuncDataTemplate<BehaviorSubject<int>>((count, _) => {
-                Button()
-                    .OnClick(o => o.Subscribe(_ => count.OnNext(count.Value + 1)))
-                    .Content("Count").Ref(out var button);
-                Label()
-                    .HorizontalAlignmentCenter().VerticalAlignmentCenter().MinWidth(24)
-                    .Content(count.ToBinding()).Ref(out var label);
-                return StackPanel()
-                    .OrientationHorizontal().Spacing(12)
-                    .HorizontalAlignmentCenter().VerticalAlignmentCenter()
-                    .Children(label, button);
-            }, true))
-            .Items(counters)
-            .Ref(out var itemsControl);
-        Window()
+        Window(out var window)
             .Title("Counters")
             .Padding(12).SizeToContentManual().Width(300).Height(300)
-            .Content(itemsControl).Ref(out var window);
+            .Content(
+                ItemsControl()
+                    .ItemTemplate(new FuncDataTemplate<BehaviorSubject<int>>((count, _) => {
+                        Button(out var button)
+                            .OnClick(o => o.Subscribe(_ => count.OnNext(count.Value + 1)))
+                            .Content("Count");
+                        Label(out var label)
+                            .HorizontalAlignmentCenter().VerticalAlignmentCenter().MinWidth(24)
+                            .Content(count.ToBinding());
+                        return StackPanel()
+                            .OrientationHorizontal().Spacing(12)
+                            .HorizontalAlignmentCenter().VerticalAlignmentCenter()
+                            .Children(label, button);
+                    }, true))
+                    .Items(counters));
         desktop.MainWindow = window;
     }, args);
