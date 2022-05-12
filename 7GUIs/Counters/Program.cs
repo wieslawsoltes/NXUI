353822d3a017472d0e3018d9373f6d8d88e2690b
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-05-12 13:23:50 +0200

    Update Program.cs

--- 7GUIs/Counters/Program.cs
+++ 7GUIs/Counters/Program.cs
@@ -1,25 +1,26 @@
-﻿AppBuilder.Configure<Application>()
+﻿var counters = Enumerable.Range(0, 5).Select(i => new BehaviorSubject<int>(i));
+
+Window Build()
+    => Window()
+        .Title("Counters")
+        .Padding(12).SizeToContentManual().Width(300).Height(300)
+        .Content(
+            ItemsControl()
+                .ItemTemplate(new FuncDataTemplate<BehaviorSubject<int>>((count, _) => 
+                    StackPanel()
+                        .OrientationHorizontal().Spacing(12).HorizontalAlignmentCenter().VerticalAlignmentCenter()
+                        .Children(
+                            Label()
+                                .HorizontalAlignmentCenter().VerticalAlignmentCenter().MinWidth(24)
+                                .Content(count.ToBinding()), 
+                            Button()
+                                .OnClick(o => o.Subscribe(_ => count.OnNext(count.Value + 1)))
+                                .Content("Count"))
+                    ,true))
+                .Items(counters));
+
+AppBuilder.Configure<Application>()
     .UsePlatformDetect()
     .UseFluentTheme()
-    .StartWithClassicDesktopLifetime(desktop => {
-        var counters = Enumerable.Range(0, 5).Select(i => new BehaviorSubject<int>(i));
-        Window(out var window)
-            .Title("Counters")
-            .Padding(12).SizeToContentManual().Width(300).Height(300)
-            .Content(
-                ItemsControl()
-                    .ItemTemplate(new FuncDataTemplate<BehaviorSubject<int>>((count, _) => {
-                        Button(out var button)
-                            .OnClick(o => o.Subscribe(_ => count.OnNext(count.Value + 1)))
-                            .Content("Count");
-                        Label(out var label)
-                            .HorizontalAlignmentCenter().VerticalAlignmentCenter().MinWidth(24)
-                            .Content(count.ToBinding());
-                        return StackPanel()
-                            .OrientationHorizontal().Spacing(12)
-                            .HorizontalAlignmentCenter().VerticalAlignmentCenter()
-                            .Children(label, button);
-                    }, true))
-                    .Items(counters));
-        desktop.MainWindow = window;
-    }, args);
+    .WithApplicationName("Counters")
+    .StartWithClassicDesktopLifetime(Build, args);
