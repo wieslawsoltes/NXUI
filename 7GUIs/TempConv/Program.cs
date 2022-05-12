707b77f9dad8adf58aba528dc5bfb2b15009331b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-05-13 00:02:23 +0200

    Add initial TempConv project

---
+++ 7GUIs/TempConv/Program.cs
@@ -0,0 +1,52 @@
+﻿var celsius = new Subject<string?>();
+var fahrenheit = new Subject<string?>();
+
+Window Build() 
+{
+    Window(out var window)
+        .Title("TempConv").Padding(12).Width(350).Height(200)
+        .Content(
+            StackPanel()
+                .OrientationHorizontal().Spacing(12).HorizontalAlignmentCenter().VerticalAlignmentCenter()
+                .Children(
+                    TextBox(out var tc)
+                        .Text(celsius),
+                    Label()
+                        .HorizontalAlignmentCenter().VerticalAlignmentCenter()
+                        .Content("Celsius = "),
+                    TextBox(out var tf)
+                        .Text(fahrenheit),
+                    Label()
+                        .HorizontalAlignmentCenter().VerticalAlignmentCenter()
+                        .Content("Fahrenheit")));
+
+    tc.ObserveText().Subscribe(x =>
+    {
+        if (x is null) return;
+        if (!double.TryParse(x, out var c))
+        {
+            celsius.OnError(new Exception());
+            return;
+        }
+        var f = Math.Round(c * (9d / 5d) + 32d);
+        fahrenheit.OnNext($"{f}");
+        Debug.WriteLine($"Celsius = {x}");
+    });
+
+    tf.ObserveText().Subscribe(x =>
+    {
+        if (x is null) return;
+        if (!double.TryParse(x, out var f)) return;
+        var c = Math.Round((f - 32d) * (5d / 9d));
+        celsius.OnNext($"{c}");
+        Debug.WriteLine($"Fahrenheit = {f}");
+    });
+
+    return window;
+}
+
+AppBuilder.Configure<Application>()
+    .UsePlatformDetect()
+    .UseFluentTheme()
+    .WithApplicationName("TempConv")
+    .StartWithClassicDesktopLifetime(Build, args);
