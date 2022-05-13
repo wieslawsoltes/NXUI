c3e6397e8eb76d3b693afa12ea1f0840b866f4e5
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-05-13 20:32:28 +0200

    Update TemplatedControlExtensions.cs

--- src/MinimalAvalonia/Extensions/TemplatedControlExtensions.cs
+++ src/MinimalAvalonia/Extensions/TemplatedControlExtensions.cs
@@ -2,22 +2,6 @@ namespace MinimalAvalonia.Extensions;
 
 public static partial class TemplatedControlExtensions
 {
-    // BackgroundProperty
-
-    public static T Background<T>(this T templatedControl, IBrush background) where T : TemplatedControl
-    {
-        templatedControl[Avalonia.Controls.Primitives.TemplatedControl.BackgroundProperty] = background;
-        return templatedControl;
-    }
-
-    // BorderBrushProperty
-
-    public static T BorderBrush<T>(this T templatedControl, IBrush borderBrush) where T : TemplatedControl
-    {
-        templatedControl[Avalonia.Controls.Primitives.TemplatedControl.BorderBrushProperty] = borderBrush;
-        return templatedControl;
-    }
-
     // BorderThicknessProperty
 
     public static T BorderThickness<T>(this T templatedControl, double uniformLength) where T : TemplatedControl
@@ -55,46 +39,6 @@ public static partial class TemplatedControlExtensions
     public static T CornerRadius<T>(this T templatedControl, double topLeft, double topRight, double bottomRight, double bottomLeft) where T : TemplatedControl
     {
         templatedControl[Avalonia.Controls.Primitives.TemplatedControl.CornerRadiusProperty] = new CornerRadius(topLeft, topRight, bottomRight, bottomLeft);
-        return templatedControl;
-    }
-
-    // FontFamilyProperty
-
-    public static T FontFamily<T>(this T templatedControl, FontFamily fontFamily) where T : TemplatedControl
-    {
-        templatedControl[Avalonia.Controls.Primitives.TemplatedControl.FontFamilyProperty] = fontFamily;
-        return templatedControl;
-    }
-
-    // FontSizeProperty
-
-    public static T FontSize<T>(this T templatedControl, double fontSize) where T : TemplatedControl
-    {
-        templatedControl[Avalonia.Controls.Primitives.TemplatedControl.FontSizeProperty] = fontSize;
-        return templatedControl;
-    }
-
-    // FontStyleProperty
-
-    public static T FontStyle<T>(this T templatedControl, FontStyle fontStyle) where T : TemplatedControl
-    {
-        templatedControl[Avalonia.Controls.Primitives.TemplatedControl.FontStyleProperty] = fontStyle;
-        return templatedControl;
-    }
-
-    // FontWeightProperty
-
-    public static T FontWeight<T>(this T templatedControl, FontWeight fontWeight) where T : TemplatedControl
-    {
-        templatedControl[Avalonia.Controls.Primitives.TemplatedControl.FontWeightProperty] = fontWeight;
-        return templatedControl;
-    }
-
-    // ForegroundProperty
-
-    public static T Foreground<T>(this T templatedControl, IBrush foreground) where T : TemplatedControl
-    {
-        templatedControl[Avalonia.Controls.Primitives.TemplatedControl.ForegroundProperty] = foreground;
         return templatedControl;
     }
 
