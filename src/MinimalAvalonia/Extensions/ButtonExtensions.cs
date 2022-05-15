be1848dfb1c73b2438f10d04edde833572175bdf
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-05-15 11:57:38 +0200

    Update ButtonExtensions.cs

--- src/MinimalAvalonia/Extensions/ButtonExtensions.cs
+++ src/MinimalAvalonia/Extensions/ButtonExtensions.cs
@@ -4,23 +4,25 @@ public static partial class ButtonExtensions
 {
     // Click
 
-    public static Button OnClick(this Button button, Action<Button, IObservable<RoutedEventArgs>> handler)
+    public static Button OnClickHandler(this Button obj, Action<Button, RoutedEventArgs> action, Avalonia.Interactivity.RoutingStrategies routes = Avalonia.Interactivity.RoutingStrategies.Bubble)
     {
-        var observable = Observable
-            .FromEventPattern<EventHandler<RoutedEventArgs>, RoutedEventArgs>(
-                h => button.Click += h, 
-                h => button.Click -= h)
-            .Select(x => x.EventArgs);
-        handler(button, observable);
-        return button;
+        obj.AddHandler(Avalonia.Controls.Button.ClickEvent, (_, args) => action(obj, args), Avalonia.Interactivity.RoutingStrategies.Bubble);
+        return obj;
     }
 
-    public static IObservable<RoutedEventArgs> OnClick(this Button button)
+    public static Button OnClick(this Button obj, Action<Button, IObservable<RoutedEventArgs>> handler,  Avalonia.Interactivity.RoutingStrategies routes = Avalonia.Interactivity.RoutingStrategies.Bubble)
+    {
+        var observable = obj.GetObservable(Avalonia.Controls.Button.ClickEvent, routes);
+        handler(obj, observable);
+        return obj;
+    }
+
+    public static IObservable<RoutedEventArgs> OnClick(this Button obj)
     {
         return Observable
             .FromEventPattern<EventHandler<RoutedEventArgs>, RoutedEventArgs>(
-                h => button.Click += h, 
-                h => button.Click -= h)
+                h => obj.Click += h, 
+                h => obj.Click -= h)
             .Select(x => x.EventArgs);
     }
 }
