50420a86410409943e9a3dc0ff65f4e720c171a0
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-05-15 12:26:21 +0200

    Update Program.cs

--- samples/7GUIs/TempConv/Program.cs
+++ samples/7GUIs/TempConv/Program.cs
@@ -12,15 +12,12 @@ Window Build()
                         .Text(celsius.Select(x => x.ToString()))
                         .OnText((tc, o) => o.Subscribe(x => {
                             tc.Errors(Enumerable.Empty<string>());
-                            if (string.IsNullOrWhiteSpace(x)) {
-                                return;
-                            }
+                            if (string.IsNullOrWhiteSpace(x)) return;
                             if (!double.TryParse(x, out var c)) {
                                 tc.Errors(new[] { "Invalid number." });
                                 return;
                             }
-                            var f = Math.Round(c * (9d / 5d) + 32d);
-                            fahrenheit.OnNext(f);
+                            fahrenheit.OnNext(Math.Round(c * (9d / 5d) + 32d));
                         })),
                     Label()
                         .HorizontalAlignmentCenter().VerticalAlignmentCenter()
@@ -29,15 +26,12 @@ Window Build()
                         .Text(fahrenheit.Select(x => x.ToString()))
                         .OnText((tf, o) => o.Subscribe(x => {
                             tf.Errors(Enumerable.Empty<string>());
-                            if (string.IsNullOrWhiteSpace(x)) {
-                                return;
-                            }
+                            if (string.IsNullOrWhiteSpace(x)) return;
                             if (!double.TryParse(x, out var f)) {
                                 tf.Errors(new[] { "Invalid number." });
                                 return;
                             }
-                            var c = Math.Round((f - 32d) * (5d / 9d));
-                            celsius.OnNext(c);
+                            celsius.OnNext(Math.Round((f - 32d) * (5d / 9d)));
                         })),
                     Label()
                         .HorizontalAlignmentCenter().VerticalAlignmentCenter()
