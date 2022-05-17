0b06936f0e2544635ae92986f585d2a4298c49ca
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-05-17 11:02:29 +0200

    Refactor

--- src/Generator/Factory.cs
+++ src/Generator/Factory.cs
@@ -1,6 +1,5 @@
 ﻿using System.Reflection;
 using Avalonia;
-using Avalonia.Interactivity;
 using Generator.Model;
 
 namespace Generator;
@@ -11,15 +10,6 @@ internal static class Factory
     {
         "AboutAvaloniaDialog"
     };
-
-    private static readonly FieldInfo? s_registered = 
-        typeof(AvaloniaPropertyRegistry).GetField("_registered", BindingFlags.NonPublic | BindingFlags.Instance);
-
-    private static readonly FieldInfo? s_attached = 
-        typeof(AvaloniaPropertyRegistry).GetField("_attached", BindingFlags.NonPublic | BindingFlags.Instance);
-
-    private static readonly FieldInfo? s_registeredRoutedEvents = 
-        typeof(RoutedEventRegistry).GetField("_registeredRoutedEvents", BindingFlags.NonPublic | BindingFlags.Instance);
 
     private static string FixType(string t)
     {
@@ -53,28 +43,159 @@ internal static class Factory
            select assemblyType).ToArray();
     }
 
+    private static List<Property> GetProperties(Type classType, Registry registry)
+    {
+        var properties = new List<Property>();
+
+        if (registry.RegisteredProperties is null)
+        {
+            return properties;
+        }
+
+        registry.RegisteredProperties.TryGetValue(classType, out var registeredPropertiesDict);
+        if (registeredPropertiesDict is null)
+        {
+            return properties;
+        }
+
+        var avaloniaProperties = registeredPropertiesDict.Values;
+
+        foreach (var property in avaloniaProperties)
+        {
+            var fieldInfo = classType.GetField($"{property.Name}Property");
+            if (fieldInfo is null)
+                continue;
+
+            if (!fieldInfo.IsPublic)
+                continue;
+
+            if (fieldInfo.GetCustomAttributes().Any(x => x.GetType().Name == "ObsoleteAttribute"))
+                continue;
+
+            if (!property.PropertyType.IsPublic)
+                continue;
+
+            var propertyType = fieldInfo.FieldType; // property.GetType()
+            var valueType = property.PropertyType;
+            var ownerType = property.OwnerType;
+
+            if (property.IsAttached 
+                // && property.GetType() == fieldInfo.FieldType 
+                && registry.RegisteredAttachedProperties is { })
+            {
+                foreach (var kvp1 in registry.RegisteredAttachedProperties)
+                {
+                    foreach (var kvp2 in kvp1.Value)
+                    {
+                        if (kvp2.Value == property)
+                        {
+                            ownerType = kvp1.Key;
+                        }
+                    }
+                }
+            }
+
+            var alreadyExists = false;
+
+            if (property.OwnerType != classType)
+            {
+                var t = classType;
+
+                while (t != null)
+                {
+                    if (ownerType == t)
+                    {
+                        alreadyExists = true;
+                        break;
+                    }
+
+                    t = t.BaseType;
+                }
+
+                ownerType = classType;
+            }
+
+            var isEnum = false;
+            var enumNames = new List<string>();
+
+            if (property.PropertyType.BaseType == typeof(Enum))
+            {
+                var names = Enum.GetNames(property.PropertyType);
+                enumNames.AddRange(names);
+                isEnum = true;
+            }
+
+            properties.Add(new Property(
+                property.Name,
+                FixType(ownerType.ToString()),
+                FixType(valueType.ToString()),
+                FixType(propertyType.ToString()),
+                alreadyExists,
+                property.IsReadOnly,
+                isEnum,
+                isEnum ? enumNames.ToArray() : null));
+        }
+
+        return properties;
+    }
+    
+    private static List<Event> GetRoutedEvents(Type classType, Registry registry)
+    {
+        var events = new List<Event>();
+
+        if (registry.RegisteredRoutedEvents is null)
+        {
+            return events;
+        }
+
+        registry.RegisteredRoutedEvents.TryGetValue(classType, out var registeredRoutedEventsDict);
+        if (registeredRoutedEventsDict is null)
+        {
+            return events;
+        }
+
+        var avaloniaRoutedEvents = registeredRoutedEventsDict;
+ 
+        foreach (var routedEvent in avaloniaRoutedEvents)
+        {
+            var fieldInfo = classType.GetField($"{routedEvent.Name}Event");
+            if (fieldInfo is null)
+                continue;
+
+            if (!fieldInfo.IsPublic)
+                continue;
+
+            if (fieldInfo.GetCustomAttributes().Any(x => x.GetType().Name == "ObsoleteAttribute"))
+                continue;
+
+            if (!routedEvent.EventArgsType.IsPublic)
+                continue;
+
+            var eventType = fieldInfo.FieldType; // property.GetType()
+            var argsType = routedEvent.EventArgsType;
+            var ownerType = routedEvent.OwnerType;
+            var routingStrategies = routedEvent.RoutingStrategies;
+
+            events.Add(new Event(
+                routedEvent.Name,
+                FixType(ownerType.ToString()),
+                FixType(argsType.ToString()),
+                FixType(eventType.ToString()),
+                routingStrategies.ToString()));
+        }
+
+        return events;
+    }
+
     public static List<Class>? CreateClasses()
     {
         var classTypes = GetClassTypes();
         var classes = new List<Class>();
 
-        foreach (var classType in classTypes)
-        {
-            System.Runtime.CompilerServices.RuntimeHelpers.RunClassConstructor(classType.TypeHandle);
-        }
-
-        var registeredProperties = (Dictionary<Type, Dictionary<int, AvaloniaProperty>>?)
-            s_registered?.GetValue(AvaloniaPropertyRegistry.Instance);
-
-        var registeredAttachedProperties = (Dictionary<Type, Dictionary<int, AvaloniaProperty>>?)
-            s_attached?.GetValue(AvaloniaPropertyRegistry.Instance);
-
-        var registeredRoutedEvents = (Dictionary<Type, List<RoutedEvent>>?)
-            s_registeredRoutedEvents?.GetValue(RoutedEventRegistry.Instance);
-
-        if (registeredProperties is null 
-            || registeredAttachedProperties is null 
-            || registeredRoutedEvents is null)
+        var registry = new Registry(classTypes);
+        if (registry.RegisteredProperties is null 
+            || registry.RegisteredAttachedProperties is null 
+            || registry.RegisteredRoutedEvents is null)
         {
             return null;
         }
@@ -86,143 +207,20 @@ internal static class Factory
                 continue;
             }
 
-            var properties = new List<Property>();
-            var events = new List<Event>();
+            var publicCtor = classType.GetConstructors().Any(x => x.IsPublic && x.GetParameters().Length == 0);
 
-            registeredProperties.TryGetValue(classType, out var registeredPropertiesDict);
-            if (registeredPropertiesDict is { })
-            {
-                var avaloniaProperties = registeredPropertiesDict.Values;
+            var properties = GetProperties(classType, registry);
 
-                foreach (var property in avaloniaProperties)
-                {
-                    var fieldInfo = classType.GetField($"{property.Name}Property");
-                    if (fieldInfo is null)
-                        continue;
+            var routedEvents = GetRoutedEvents(classType, registry);
 
-                    if (!fieldInfo.IsPublic)
-                        continue;
-
-                    if (fieldInfo.GetCustomAttributes().Any(x => x.GetType().Name == "ObsoleteAttribute"))
-                        continue;
-
-                    if (!property.PropertyType.IsPublic)
-                        continue;
-
-                    var propertyType = fieldInfo.FieldType; // property.GetType()
-                    var valueType = property.PropertyType;
-                    var ownerType = property.OwnerType;
-
-                    if (property.IsAttached 
-                        // && property.GetType() == fieldInfo.FieldType 
-                        && registeredAttachedProperties is { })
-                    {
-                        foreach (var kvp1 in registeredAttachedProperties)
-                        {
-                            foreach (var kvp2 in kvp1.Value)
-                            {
-                                if (kvp2.Value == property)
-                                {
-                                    ownerType = kvp1.Key;
-                                }
-                            }
-                        }
-                    }
-
-                    var alreadyExists = false;
-
-                    if (property.OwnerType != classType)
-                    {
-                        var t = classType;
-
-                        while (t != null)
-                        {
-                            if (ownerType == t)
-                            {
-                                alreadyExists = true;
-                                break;
-                            }
-
-                            t = t.BaseType;
-                        }
-
-                        ownerType = classType;
-                    }
-
-                    var isEnum = false;
-                    var enumNames = new List<string>();
-
-                    if (property.PropertyType.BaseType == typeof(Enum))
-                    {
-                        var names = Enum.GetNames(property.PropertyType);
-                        enumNames.AddRange(names);
-                        isEnum = true;
-                    }
-
-                    var p = new Property(
-                        property.Name,
-                        FixType(ownerType.ToString()),
-                        FixType(valueType.ToString()),
-                        FixType(propertyType.ToString()),
-                        alreadyExists,
-                        property.IsReadOnly,
-                        isEnum,
-                        isEnum ? enumNames.ToArray() : null);
-
-                    properties.Add(p);
-                }
-            }
-
-            registeredRoutedEvents.TryGetValue(classType, out var registeredRoutedEventsDict);
-            if (registeredRoutedEventsDict is { })
-            {
-                var avaloniaRoutedEvents = registeredRoutedEventsDict;
- 
-                foreach (var @event in avaloniaRoutedEvents)
-                {
-                    var fieldInfo = classType.GetField($"{@event.Name}Event");
-                    if (fieldInfo is null)
-                        continue;
-
-                    if (!fieldInfo.IsPublic)
-                        continue;
-
-                    if (fieldInfo.GetCustomAttributes().Any(x => x.GetType().Name == "ObsoleteAttribute"))
-                        continue;
-
-                    if (!@event.EventArgsType.IsPublic)
-                        continue;
-
-                    var eventType = fieldInfo.FieldType; // property.GetType()
-                    var argsType = @event.EventArgsType;
-                    var ownerType = @event.OwnerType;
-                    var routingStrategies = @event.RoutingStrategies;
-
-                    var e = new Event(
-                        @event.Name,
-                        FixType(ownerType.ToString()),
-                        FixType(argsType.ToString()),
-                        FixType(eventType.ToString()),
-                        routingStrategies.ToString());
-
-                    events.Add(e);
-                }
-            }
-
-            var publicCtor = classType
-                .GetConstructors()
-                .Any(x => x.IsPublic && x.GetParameters().Length == 0);
-
-            var c = new Class(
+            classes.Add(new Class(
                 FixClassNameType(classType.Name), 
                 FixType(classType.ToString()), 
                 properties.ToArray(),
-                events.ToArray(),
+                routedEvents.ToArray(),
                 classType.IsSealed,
                 publicCtor,
-                classType.IsAbstract);
-
-            classes.Add(c);
+                classType.IsAbstract));
         }
 
         return classes;
