15577cd9415e15b8b38252f9ef3a98c7c70c1046
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-05-17 11:02:16 +0200

    Create Registry.cs

---
+++ src/Generator/Registry.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using Avalonia;
+using Avalonia.Interactivity;
+
+namespace Generator;
+
+internal class Registry
+{
+    private static readonly FieldInfo? s_registered = 
+        typeof(AvaloniaPropertyRegistry).GetField("_registered", BindingFlags.NonPublic | BindingFlags.Instance);
+
+    private static readonly FieldInfo? s_attached = 
+        typeof(AvaloniaPropertyRegistry).GetField("_attached", BindingFlags.NonPublic | BindingFlags.Instance);
+
+    private static readonly FieldInfo? s_registeredRoutedEvents = 
+        typeof(RoutedEventRegistry).GetField("_registeredRoutedEvents", BindingFlags.NonPublic | BindingFlags.Instance);
+
+    public Dictionary<Type, Dictionary<int, AvaloniaProperty>>? RegisteredProperties { get; }
+
+    public Dictionary<Type, Dictionary<int, AvaloniaProperty>>? RegisteredAttachedProperties { get; }
+
+    public Dictionary<Type, List<Avalonia.Interactivity.RoutedEvent>>? RegisteredRoutedEvents { get; }
+
+    public Registry(Type[] classTypes)
+    {
+        foreach (var classType in classTypes)
+        {
+            System.Runtime.CompilerServices.RuntimeHelpers.RunClassConstructor(classType.TypeHandle);
+        }
+
+        RegisteredProperties = (Dictionary<Type, Dictionary<int, AvaloniaProperty>>?)
+            s_registered?.GetValue(AvaloniaPropertyRegistry.Instance);
+
+        RegisteredAttachedProperties = (Dictionary<Type, Dictionary<int, AvaloniaProperty>>?)
+            s_attached?.GetValue(AvaloniaPropertyRegistry.Instance);
+
+        RegisteredRoutedEvents = (Dictionary<Type, List<Avalonia.Interactivity.RoutedEvent>>?)
+            s_registeredRoutedEvents?.GetValue(RoutedEventRegistry.Instance);
+    }
+}
