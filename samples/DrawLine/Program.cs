addb931c3bb75d655f46ceec4c48e3694beaea4a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-05-17 23:20:08 +0200

    Update Program.cs

--- samples/DrawLine/Program.cs
+++ samples/DrawLine/Program.cs
@@ -1,30 +1,32 @@
 ﻿Window Build() 
     => Window()
         .Title("DrawLine").Width(500).Height(400)
-        .Content(
-            Canvas()
-                .Background(Brushes.WhiteSmoke)
-                .Var(default(Line), out var line)
-                .OnPointerPressed((canvas, o)
-                    => o.Select(x => x.GetPosition(canvas)).Subscribe(x =>
-                    {
-                        canvas.Children(line = Line().StartPoint(x).EndPoint(x));
-                    }))
-                .OnPointerReleased((canvas, o)
-                    => o.Select(x => x.GetPosition(canvas)).Subscribe(_ =>
-                    {
-                        line = null;
-                    }))
-                .OnPointerMoved((canvas, o)
-                    => o.Select(x => x.GetPosition(canvas)).Subscribe(x =>
-                    {
-                        line?.EndPoint(x);
-                    }))
-                .Styles(
-                    Style()
-                        .Selector(x => x.OfType<Line>())
-                        .Setter(ShapeStroke, Brushes.Black)
-                        .Setter(ShapeStrokeThickness, 2d)));
+        .Content(MainView());
+
+Control MainView()
+    => Canvas()
+        .Background(Brushes.WhiteSmoke)
+        .Var(default(Line), out var line)
+        .OnPointerPressed((canvas, o)
+            => o.Select(x => x.GetPosition(canvas)).Subscribe(x =>
+            {
+                canvas.Children(line = Line().StartPoint(x).EndPoint(x));
+            }))
+        .OnPointerReleased((canvas, o)
+            => o.Select(x => x.GetPosition(canvas)).Subscribe(_ =>
+            {
+                line = null;
+            }))
+        .OnPointerMoved((canvas, o)
+            => o.Select(x => x.GetPosition(canvas)).Subscribe(x =>
+            {
+                line?.EndPoint(x);
+            }))
+        .Styles(
+            Style()
+                .Selector(x => x.OfType<Line>())
+                .Setter(ShapeStroke, Brushes.Black)
+                .Setter(ShapeStrokeThickness, 2d));
 
 AppBuilder.Configure<Application>()
     .UsePlatformDetect()
