a4443a67aca6b5e4b344835a03903199201ad640
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-05-19 23:48:29 +0200

    Update Program.cs

--- samples/DrawLine/Program.cs
+++ samples/DrawLine/Program.cs
@@ -3,11 +3,6 @@
         .Title("DrawLine").Width(500).Height(400)
         .Content(MainView());
 
-var strokeThickness = 1d;
-var strokeThicknessObservable = Observable
-    .Timer(TimeSpan.Zero, TimeSpan.FromSeconds(1), AvaloniaScheduler.Instance)
-    .Select(_ => strokeThickness += 0.1d);
-
 Control MainView()
     => Canvas()
         .Background(Brushes.WhiteSmoke)
@@ -15,7 +10,7 @@ Control MainView()
         .OnPointerPressed((canvas, o)
             => o.Select(x => x.GetPosition(canvas)).Subscribe(x =>
             {
-                canvas.Children(line = Line().StartPoint(x).EndPoint(x));
+                canvas.Children(line = Line().Styles(LineStyle()).StartPoint(x).EndPoint(x));
             }))
         .OnPointerReleased((canvas, o)
             => o.Select(x => x.GetPosition(canvas)).Subscribe(_ =>
@@ -26,15 +21,18 @@ Control MainView()
             => o.Select(x => x.GetPosition(canvas)).Subscribe(x =>
             {
                 line?.EndPoint(x);
-            }))
-        .Styles(
-            LineStyle());
+            }));
 
-Style LineStyle() 
-    => Style()
+Style LineStyle() {
+    var strokeThickness = 1d;
+    var strokeThicknessObservable = Observable
+        .Timer(TimeSpan.Zero, TimeSpan.FromSeconds(1), AvaloniaScheduler.Instance)
+        .Select(_ => strokeThickness += 0.5d);
+    return Style()
         .Selector(x => x.OfType<Line>())
         .SetShapeStroke(Brushes.Red)
         .SetShapeStrokeThickness(strokeThicknessObservable);
+}
 
 AppBuilder.Configure<Application>()
     .UsePlatformDetect()
