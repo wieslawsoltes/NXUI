32b08fd5d8fcbfa5ce860937554919aa82184a79
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-05-21 22:15:51 +0200

    Create Log.cs

---
+++ src/Generator/Log.cs
@@ -0,0 +1,20 @@
+namespace Generator;
+
+internal static class Log
+{
+    public static void Info(string message)
+    {
+        var color = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("[INFO]\t" + message);
+        Console.ForegroundColor = color;
+    }
+
+    public static void Error(string message)
+    {
+        var color = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("[ERROR]\t" + message);
+        Console.ForegroundColor = color;
+    }
+}
