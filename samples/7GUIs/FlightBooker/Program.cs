b881d3b0abaf70044e99220f690d857b442e3670
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-05-24 23:14:23 +0200

    Create project FlightBooker

---
+++ samples/7GUIs/FlightBooker/Program.cs
@@ -0,0 +1,35 @@
+﻿var start = new BehaviorSubject<string>(DateTime.Now.ToString("dd.MM.yyyy"));
+var end = new BehaviorSubject<string>(DateTime.Now.ToString("dd.MM.yyyy"));
+var selected = new BehaviorSubject<int>(0);
+
+Window Build() 
+    => Window()
+        .Title("Book Flight").Padding(12).Width(450).Height(200)
+        .Content(
+            StackPanel()
+                .OrientationVertical().Spacing(12).HorizontalAlignmentCenter().VerticalAlignmentCenter()
+                .Children(
+                    ComboBox()
+                        .Items(new [] { "one-way flight", "return flight" })
+                        .SelectedIndex(selected),
+                    TextBox()
+                        .Text(start)
+                        .OnText((t1, o) => o.Subscribe(x => {
+                            // TODO:
+                        })),
+                    TextBox()
+                        .Text(end)
+                        .OnText((t2, o) => o.Subscribe(x => {
+                            // TODO:
+                        })),
+                    Button()
+                        .Content("Book")
+                        .OnClick((_, o) => o.Subscribe(_ => {
+                            // TODO:
+                        }))));
+
+AppBuilder.Configure<Application>()
+    .UsePlatformDetect()
+    .UseFluentTheme()
+    .WithApplicationName("FlightBooker")
+    .StartWithClassicDesktopLifetime(Build, args);
