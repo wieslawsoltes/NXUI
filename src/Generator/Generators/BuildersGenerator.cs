c403cc8d9588faf192e945df79b786cdb2f99383
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-10-11 09:50:53 +0200

    Update BuildersGenerator.cs

--- src/Generator/Generators/BuildersGenerator.cs
+++ src/Generator/Generators/BuildersGenerator.cs
@@ -8,14 +8,6 @@ internal static class BuildersGenerator
 {
     public static void Generate(string outputPath, List<Class> classes)
     {
-        var outputFile = Path.Combine(outputPath, $"Builders.g.cs");
-
-        using var file = File.CreateText(outputFile);
-        void WriteLine(string x) => file.WriteLine(x);
-
-        var fileHeaderBuilder = new StringBuilder(Templates.BuildersHeaderTemplate);
-        WriteLine(fileHeaderBuilder.ToString());
-
         for (var i = 0; i < classes.Count; i++)
         {
             var c = classes[i];
@@ -30,18 +22,21 @@ internal static class BuildersGenerator
                 continue;
             }
 
+            var outputFile = Path.Combine(outputPath, $"{c.Name}.Builders.g.cs");
+
+            using var file = File.CreateText(outputFile);
+            void WriteLine(string x) => file.WriteLine(x);
+
+            var fileHeaderBuilder = new StringBuilder(Templates.BuildersHeaderTemplate);
+            WriteLine(fileHeaderBuilder.ToString());
+
             var buildersBuilder = new StringBuilder(Templates.BuildersTemplate);
             buildersBuilder.Replace("%ClassName%", c.Name);
             buildersBuilder.Replace("%ClassType%", c.Type);
             WriteLine(buildersBuilder.ToString());
 
-            if (i < classes.Count - 1)
-            {
-                WriteLine("");
-            }
+            var classFooterBuilder = new StringBuilder(Templates.BuildersFooterTemplate);
+            WriteLine(classFooterBuilder.ToString());
         }
-
-        var classFooterBuilder = new StringBuilder(Templates.BuildersFooterTemplate);
-        WriteLine(classFooterBuilder.ToString());
     }
 }
