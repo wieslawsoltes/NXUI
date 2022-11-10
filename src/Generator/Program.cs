93c761794a072d76040c77acec163397787433cb
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-11-10 13:43:17 +0100

    Update Program.cs

--- src/Generator/Program.cs
+++ src/Generator/Program.cs
@@ -1,22 +1,14 @@
 ﻿using Avalonia;
-using Avalonia.Headless;
-using Avalonia.Themes.Fluent;
 using Generator;
 
 AppBuilder.Configure<Application>()
     .UsePlatformDetect()
     .AfterSetup(x =>
     {
-        var theme = new FluentTheme(new Uri($"avares://{System.Reflection.Assembly.GetExecutingAssembly().GetName()}"))
-        {
-            Mode = FluentThemeMode.Light
-        };
-        x.Instance?.Styles.Add(theme);
-
         if (args.Length == 1)
         {
             MinimalGenerator.Generate(args[0]);
         }
     })
-    .UseHeadless(new AvaloniaHeadlessPlatformOptions { UseCompositor = false, UseHeadlessDrawing = false }).SetupWithoutStarting();
+    .SetupWithoutStarting();
 
