5ea1973ecb0a03379c0895bc3db566460fef886a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-02-04 23:04:57 +0100

    Add minimalistic Run method

--- samples/NXUI.Sample.Desktop/App.cs
+++ samples/NXUI.Sample.Desktop/App.cs
@@ -1,23 +1,5 @@
-var count = 0;
-
-Window Build()
-  => Window(out var window)
-    .Title("NXUI").Width(400).Height(300)
-    .Content(
-      StackPanel()
-        .Children(
-          Button(out var button)
-            .Content("Welcome to Avalonia, please click me!"),
-          TextBox(out var tb1)
-            .Text("NXUI"),
-          TextBox()
-            .Text(window.BindTitle()),
-          Label()
-            .Content(button.ObserveOnClick().Select(_ => ++count).Select(x => $"You clicked {x} times."))))
-    .Title(tb1.ObserveText().Select(x => x?.ToUpper()));
-
-AppBuilder.Configure<Application>()
-  .UsePlatformDetect()
-  .UseFluentTheme()
-  .WithApplicationName("NXUI")
-  .StartWithClassicDesktopLifetime(Build, args);
+Run(
+  () => Window().Content(Label().Content("NXUI")), 
+  "NXUI", 
+  args, 
+  ThemeVariant.Dark);
---
+++ samples/NXUI.Sample.Desktop/NXUI.cs
@@ -0,0 +1,53 @@
+﻿namespace NXUI.Sample.Desktop;
+
+/// <summary>
+/// 
+/// </summary>
+public static partial class NXUI
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="callback"></param>
+    /// <param name="name"></param>
+    /// <param name="args"></param>
+    /// <param name="themeVariant"></param>
+    /// <param name="shutdownMode"></param>
+    /// <returns></returns>
+    public static int Run(
+        Action<IClassicDesktopStyleApplicationLifetime>? callback, 
+        string name,
+        string[] args, 
+        ThemeVariant? themeVariant = null,
+        ShutdownMode shutdownMode = ShutdownMode.OnLastWindowClose)
+    {
+        return AppBuilder.Configure<Application>()
+            .UsePlatformDetect()
+            .UseFluentTheme(themeVariant)
+            .WithApplicationName(name)
+            .StartWithClassicDesktopLifetime(callback, args, shutdownMode);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="callback"></param>
+    /// <param name="name"></param>
+    /// <param name="args"></param>
+    /// <param name="themeVariant"></param>
+    /// <param name="shutdownMode"></param>
+    /// <returns></returns>
+    public static int Run(
+        Func<Window>? callback, 
+        string name,
+        string[] args, 
+        ThemeVariant? themeVariant = null,
+        ShutdownMode shutdownMode = ShutdownMode.OnLastWindowClose)
+    {
+        return AppBuilder.Configure<Application>()
+            .UsePlatformDetect()
+            .UseFluentTheme(themeVariant)
+            .WithApplicationName(name)
+            .StartWithClassicDesktopLifetime(callback, args, shutdownMode);
+    }
+}
